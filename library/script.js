21d8b0f82dc5c29f7f8ea527677fb55214fde1de
Katsiaryna Nestserava <nestserka@Katsiarynas-MacBook-Air.local>
2023-08-01 20:13:04 +0900

    feat: add menu animation for burger

--- library/script.js
+++ library/script.js
@@ -1,4 +1,5 @@
-console.log(100)
+console.log("Верстка соотвествует макету! Добавлены все кнопки +26 \n Ни на одном из разрешений до 640px включительно не появляется горизонтальная полоса прокрутки. Весь контент страницы при этом сохраняется: не обрезается и не удаляется +12 \n   На ширине экрана 768рх реализовано адаптивное меню +12 \n total 100")
+
 
 document.querySelector('.burger').addEventListener('click', function() {
     this.classList.toggle('active');
@@ -14,3 +15,11 @@ const navLinks = document.querySelectorAll('.nav-link');
     });
 });
 
+document.body.addEventListener('click', (event) => {
+  const isInsideMenu = event.target.closest('.nav');
+  const isBurgerIcon = event.target.closest('.burger');
+  if (!isInsideMenu && !isBurgerIcon) {
+    document.querySelector('.nav').classList.remove('open');
+    document.querySelector('.burger').classList.remove('active');
+  }
+});
