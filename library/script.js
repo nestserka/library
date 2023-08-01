d3abab304b50b36848e925db44085a4a2e8cca97
Katsiaryna Nestserava <nestserka@Katsiarynas-MacBook-Air.local>
2023-08-01 21:45:27 +0900

    fix: remove dots from ul list in footer

--- library/script.js
+++ library/script.js
@@ -1,10 +1,27 @@
 console.log("Верстка соотвествует макету! Добавлены все кнопки +26 \n Ни на одном из разрешений до 640px включительно не появляется горизонтальная полоса прокрутки. Весь контент страницы при этом сохраняется: не обрезается и не удаляется +12 \n   На ширине экрана 768рх реализовано адаптивное меню +12 \n total 100")
 
+const burger = document.querySelector('.burger');
+const nav = document.querySelector('.nav');
 
-document.querySelector('.burger').addEventListener('click', function() {
-    this.classList.toggle('active');
-    document.querySelector('.nav').classList.toggle('open')
-})
+function closeMenu() {
+  nav.classList.remove('open');
+  burger.classList.remove('active');
+  nav.classList.add('close');
+
+  setTimeout(() => {
+    nav.classList.remove('close');
+  }, 300); 
+}
+
+
+burger.addEventListener('click', function() {
+  this.classList.toggle('active');
+  if (nav.classList.contains('open')) {
+    closeMenu();
+  } else {
+    nav.classList.add('open');
+  }
+});
 
 
 const navLinks = document.querySelectorAll('.nav-link');
@@ -12,14 +29,18 @@ const navLinks = document.querySelectorAll('.nav-link');
     link.addEventListener('click', () => {
       document.querySelector('.nav').classList.remove('open'); 
       document.querySelector('.burger').classList.remove('active');
+      nav.classList.add('close');
+
+      setTimeout(() => {
+        nav.classList.remove('close');
+      }, 300); 
     });
 });
 
 document.body.addEventListener('click', (event) => {
   const isInsideMenu = event.target.closest('.nav');
   const isBurgerIcon = event.target.closest('.burger');
-  if (!isInsideMenu && !isBurgerIcon) {
-    document.querySelector('.nav').classList.remove('open');
-    document.querySelector('.burger').classList.remove('active');
+  if (!isInsideMenu && !isBurgerIcon && nav.classList.contains('open')) {
+    closeMenu();
   }
 });
