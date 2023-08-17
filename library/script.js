61cadbf11fd83788ac42b50104e60ffb828ecdb7
nestserka <nestserava.k@gmail.com>
2023-08-17 22:02:42 +0900

    feat: add slider to main

--- library/script.js
+++ library/script.js
@@ -44,3 +44,69 @@ document.body.addEventListener('click', (event) => {
     closeMenu();
   }
 });
+
+
+// slider
+
+const btnPrev = document.querySelector('.left-arrow');
+const btnNext = document.querySelector('.right-arrow');
+
+let position = 0;
+const slidesToShow = window.innerWidth > 768 ? 3 : 1;
+const slidesToScroll = 1;
+const container = document.querySelector('.slider-container');
+const track = document.querySelector('.slider-track');
+const radioButtons = document.querySelectorAll('.picture-radio-buttons input[type="radio"]');
+const items = document.querySelectorAll('.slider-item');
+const itemsCount = items.length;
+const itemWidth = 450;
+let gap = 25;
+const movePosition = slidesToScroll * itemWidth;
+
+
+
+items.forEach((item) => {
+    item.style.minWidth =`${itemWidth}px`;
+})
+
+const setPosition = () => {
+    track.style.transform = `translateX(${position}px)`
+};
+
+radioButtons.forEach((radioButton, index) => {
+  radioButton.addEventListener('change', () => {
+      if (window.innerWidth <= 768) {
+          position = -(index * itemWidth);
+      } else {
+          position = -(index * (itemWidth + gap));
+      }
+      setPosition();
+  });
+});
+
+btnNext.addEventListener('click', () => { 
+  const itemsLeft = itemsCount - (Math.abs(position) + slidesToShow * itemWidth + (slidesToShow - 1)) / itemWidth;
+  position -= itemsLeft >= slidesToScroll ? movePosition : itemsLeft * itemWidth;
+  setPosition();
+  checkBtns();
+  const currentIndex = Math.floor(Math.abs(position) / itemWidth);
+  radioButtons[currentIndex].checked = true;
+});
+
+btnPrev.addEventListener('click', () => { 
+  const itemsLeft = Math.abs(position) / itemWidth;
+  position += itemsLeft >= slidesToScroll ? movePosition : itemsLeft * itemWidth;
+  setPosition();
+  checkBtns();
+  const currentIndex = Math.floor(Math.abs(position) / itemWidth);
+  radioButtons[currentIndex].checked = true;
+});
+
+
+const checkBtns = () => {
+    btnPrev.classList.toggle('disabled', position === 0);
+    btnNext.classList.toggle('disabled',  position <= -(itemsCount - slidesToShow) * itemWidth);
+};
+
+
+checkBtns();
