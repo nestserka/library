013873277dff48e5511d7317eff4c225ef614d63
nestserka <nechatherina@gmail.com>
2023-08-18 17:40:34 +0900

    feat: add registation form

--- library/script.js
+++ library/script.js
@@ -172,3 +172,29 @@ userProfileLink.addEventListener("click", function(event) {
     document.getElementById("register").classList.toggle("show");
     console.log("User profile link clicked!");
 });
+
+
+// let register = document.querySelector(".register-new");
+// register.addEventListener("click", function(event) {
+//   console.log("clickd on register");
+// });
+
+
+let open_modal = document.querySelectorAll('.register-new');
+let close_modal = document.getElementById('close_modal');
+let modal = document.getElementById('modal');
+let body = document.getElementsByTagName('body')[0];
+for (let i = 0; i < open_modal.length; i++) {
+    open_modal[i].onclick = function() { // клик на открытие
+        modal.classList.add('modal_vis'); // добавляем видимость окна
+        modal.classList.remove('bounceOutDown'); // удаляем эффект закрытия
+        body.classList.add('body_block'); // убираем прокрутку
+    };
+}
+close_modal.onclick = function() { // клик на закрытие
+    modal.classList.add('bounceOutDown'); // добавляем эффект закрытия
+    window.setTimeout(function() { // удаляем окно через полсекунды (чтобы увидеть эффект закрытия).
+        modal.classList.remove('modal_vis');
+        body.classList.remove('body_block'); // возвращаем прокрутку
+    }, 500);
+};
