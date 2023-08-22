cff22016cd544939eef4d7a89b470eddb5aa5fc7
nestserka <nechatherina@gmail.com>
2023-08-22 13:09:40 +0900

    fix: disable button if bought book

--- library/script.js
+++ library/script.js
@@ -12,7 +12,7 @@ let modal_login = document.getElementById('modal-login');
 let body_login = document.body;
 let close_modal_login = document.getElementById('close_modal_login');
 const navLinks = document.querySelectorAll('.nav-link');
-let  dropdownContentList = document.querySelectorAll('.dropdown-content');
+let dropdownContentList = document.querySelectorAll('.dropdown-content');
 let buyButtons = document.querySelectorAll('.buy');
 let checkIfLoggedIn = false;
 let tempKey = null;
@@ -46,7 +46,7 @@ window.onload = function () {
     let key = localStorage.key(i);
     let storedValue = localStorage.getItem(key);
     let userData = JSON.parse(storedValue);
-    if (userData.isLoggedIn === true){
+    if (userData.isLoggedIn === true) {
       checkIfLoggedIn = true;
       tempKey = userData.bookCard;
       updateProfileImage(userData);
@@ -102,7 +102,7 @@ document.body.addEventListener('click', (event) => {
   }
 
   if (!isInsideMenu && !isBurgerIcon && nav.classList.contains('open')) {
-    closeMenu(); 
+    closeMenu();
   }
 });
 
@@ -173,86 +173,87 @@ const sliderContainers = document.querySelectorAll('.books-slider-container');
 sliderContainers[0].classList.add('active');
 
 bookRadioLabels.forEach((label, index) => {
-    label.addEventListener('click', () => {
-        sliderContainers.forEach((container, i) => {
-            if (i === index) {
-                if (!container.classList.contains('active')) {
-                  setTimeout(function() {
-                    container.classList.add('active');
-                    container.classList.add("elementToFadeIn");}, 300);
-                }
-            } else {
-                if (container.classList.contains('active')) {
-                  container.classList.remove("elementToFadeIn");
-                    container.classList.add("elementToFadeInAndOut");
-                    setTimeout(function() {
-                        container.classList.remove("elementToFadeInAndOut");
-                        container.classList.remove('active');
-                    }, 300); 
-                }
-            }
-        });
+  label.addEventListener('click', () => {
+    sliderContainers.forEach((container, i) => {
+      if (i === index) {
+        if (!container.classList.contains('active')) {
+          setTimeout(function () {
+            container.classList.add('active');
+            container.classList.add("elementToFadeIn");
+          }, 300);
+        }
+      } else {
+        if (container.classList.contains('active')) {
+          container.classList.remove("elementToFadeIn");
+          container.classList.add("elementToFadeInAndOut");
+          setTimeout(function () {
+            container.classList.remove("elementToFadeInAndOut");
+            container.classList.remove('active');
+          }, 300);
+        }
+      }
     });
+  });
 });
 
 
 // registration form
 
-userProfileLink.addEventListener("click", function(event) {
-    event.preventDefault();
-    if (checkIfLoggedIn) {
+userProfileLink.addEventListener("click", function (event) {
+  event.preventDefault();
+  if (checkIfLoggedIn) {
     document.getElementById("loggedin-profile").classList.toggle("show");
     let sectionsTitle = document.querySelector(".book-number");
     sectionsTitle.textContent = tempKey;
-    } else {
+  } else {
     document.getElementById("register").classList.toggle("show");
-    }
+  }
 });
 
 for (let i = 0; i < open_modals.length; i++) {
-    open_modals[i].addEventListener('click', function() {
-        modal.classList.add('modal_vis');
-        body.classList.add('body_block');
-    });
+  open_modals[i].addEventListener('click', function () {
+    modal.classList.add('modal_vis');
+    body.classList.add('body_block');
+  });
 }
 
-close_modal.addEventListener('click', function() {
+close_modal.addEventListener('click', function () {
+  modal.classList.add('modal_fade');
+
+  setTimeout(function () {
+    modal.classList.remove('modal_vis');
+    body.classList.remove('body_block');
+    modal.classList.remove('modal_fade');
+    resetFormFields();
+  }, 1000);
+});
+
+modal.addEventListener('click', function (event) {
+  if (event.target === modal) {
+
     modal.classList.add('modal_fade');
-  
-    setTimeout(function(){
+    setTimeout(function () {
       modal.classList.remove('modal_vis');
       body.classList.remove('body_block');
       modal.classList.remove('modal_fade');
-      resetFormFields();
     }, 1000);
-});
-
-modal.addEventListener('click', function(event) {
-    if (event.target === modal) {
-
-      modal.classList.add('modal_fade');
-      setTimeout(function(){
-        modal.classList.remove('modal_vis');
-        body.classList.remove('body_block');
-        modal.classList.remove('modal_fade');
-      }, 1000);
-      resetFormFields();
-    }
+    resetFormFields();
+  }
 });
 
 
 // login
 
 for (let i = 0; i < open_login_modals.length; i++) {
-  open_login_modals[i].addEventListener('click', function() {
+  open_login_modals[i].addEventListener('click', function () {
     modal_login.classList.add('modal_vis');
     body_login.classList.add('body_block');
-    });
+  });
 }
 
-close_modal_login.addEventListener('click', function() {
+close_modal_login.addEventListener('click', function () {
   modal_login.classList.add('modal_fade');
-  setTimeout(function(){
+  setTimeout(function () {
     modal_login.classList.remove('modal_vis');
     body_login.classList.remove('body_block');
     modal_login.classList.remove('modal_fade');
@@ -260,15 +261,15 @@ close_modal_login.addEventListener('click', function() {
   }, 1000);
 });
 
-modal_login.addEventListener('click', function(event) {
+modal_login.addEventListener('click', function (event) {
   if (event.target === modal_login) {
     modal_login.classList.add('modal_fade');
-  setTimeout(function(){
-    modal_login.classList.remove('modal_vis');
-    body_login.classList.remove('body_block');
-    modal_login.classList.remove('modal_fade');
-    resetFormFields();
-  }, 1000);
+    setTimeout(function () {
+      modal_login.classList.remove('modal_vis');
+      body_login.classList.remove('body_block');
+      modal_login.classList.remove('modal_fade');
+      resetFormFields();
+    }, 1000);
   }
 });
 
@@ -276,58 +277,58 @@ modal_login.addEventListener('click', function(event) {
 // register
 
 document.addEventListener("DOMContentLoaded", function () {
-  document.getElementById("userForm").addEventListener("submit", function(event) {
-      event.preventDefault();
+  document.getElementById("userForm").addEventListener("submit", function (event) {
+    event.preventDefault();
 
-      var userName = document.getElementById("firstname").value;
-      var surname = document.getElementById("surname").value;
-      var email = document.getElementById("email").value;
-      var password = document.getElementById("password").value;
-      let size = 9;
-      var bookCard = [...Array(size)].map(() => Math.floor(Math.random() * 16).toString(16).toUpperCase()).join('');
-      var visit = 1;
-      var key = bookCard;
-      var isLoggedIn = true;
-      var booksOwn = 0;
+    var userName = document.getElementById("firstname").value;
+    var surname = document.getElementById("surname").value;
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+    let size = 9;
+    var bookCard = [...Array(size)].map(() => Math.floor(Math.random() * 16).toString(16).toUpperCase()).join('');
+    var visit = 1;
+    var key = bookCard;
+    var isLoggedIn = true;
+    var booksOwn = 0;
 
-      const user = {
-          userName,
-          surname,
-          email,
-          password,
-          visit,
-          bookCard,
-          isLoggedIn,
-          booksOwn
-      };
+    const user = {
+      userName,
+      surname,
+      email,
+      password,
+      visit,
+      bookCard,
+      isLoggedIn,
+      booksOwn
+    };
 
-      localStorage.setItem(key, JSON.stringify(user));
-      console.log("User data saved:", user);
-      checkIfLoggedIn = true;
-      tempKey = bookCard;
-      updateProfileImage(user);
-      modal.classList.add('modal_fade');
-      setTimeout(function(){
-        modal.classList.remove('modal_vis');
-        body.classList.remove('body_block');
-        modal.classList.remove('modal_fade');
-        resetFormFields();
-      }, 1000);
+    localStorage.setItem(key, JSON.stringify(user));
+    console.log("User data saved:", user);
+    checkIfLoggedIn = true;
+    tempKey = bookCard;
+    updateProfileImage(user);
+    modal.classList.add('modal_fade');
+    setTimeout(function () {
+      modal.classList.remove('modal_vis');
+      body.classList.remove('body_block');
+      modal.classList.remove('modal_fade');
+      resetFormFields();
+    }, 1000);
   });
 });
 
 let extra_modal = document.querySelector('.underline');
 
-extra_modal.addEventListener('click', function() {
-    modal.classList.remove('modal_vis');
-    body.classList.remove('body_block');
-    modal_login.classList.add('modal_vis');
-    body_login.classList.add('body_block');
+extra_modal.addEventListener('click', function () {
+  modal.classList.remove('modal_vis');
+  body.classList.remove('body_block');
+  modal_login.classList.add('modal_vis');
+  body_login.classList.add('body_block');
 });
 
 let register_button = document.querySelector('.underline-register');
 
-register_button.addEventListener('click', function() {
+register_button.addEventListener('click', function () {
   modal_login.classList.remove('modal_vis');
   body_login.classList.remove('body_block');
   modal.classList.add('modal_vis');
@@ -384,7 +385,7 @@ document.addEventListener("DOMContentLoaded", function () {
     tempKey = user.bookCard;
 
     modal_login.classList.add('modal_fade');
-    setTimeout(function(){
+    setTimeout(function () {
       modal_login.classList.remove('modal_vis');
       body_login.classList.remove('body_block');
       modal_login.classList.remove('modal_fade');
@@ -396,7 +397,7 @@ document.addEventListener("DOMContentLoaded", function () {
     console.log("Incorrect password.");
     error.textContent = message;
 
-    setTimeout(function() {
+    setTimeout(function () {
       error.textContent = "";
     }, 5000);
   }
@@ -433,30 +434,30 @@ function updateProfileImage(user) {
 
 let logoutBtn = document.querySelector(".log-out");
 
-logoutBtn.addEventListener('click', function() {
-      let storedValue = localStorage.getItem(tempKey);
-      let userData = JSON.parse(storedValue);
-      userData.isLoggedIn = false;
-      localStorage.setItem(tempKey, JSON.stringify(userData));
-      checkIfLoggedIn = false;
-      tempKey = null;
-      restoreImageOnLogout()
+logoutBtn.addEventListener('click', function () {
+  let storedValue = localStorage.getItem(tempKey);
+  let userData = JSON.parse(storedValue);
+  userData.isLoggedIn = false;
+  localStorage.setItem(tempKey, JSON.stringify(userData));
+  checkIfLoggedIn = false;
+  tempKey = null;
+  restoreImageOnLogout()
 });
 
 
 function restoreImageOnLogout() {
   const profileImage = document.getElementById('profileImage');
   profileImage.src = originalSrc;
-  profileImage.title  = "";
+  profileImage.title = "";
 }
 
 function resetFormFields() {
   loginForm.reset();
   userForm.reset();
-  cardForm.reset();
+  // cardForm.reset();
 }
 
- 
+
 // modal for user card
 
 let modalProfile = document.getElementById('modal-profile');
@@ -465,7 +466,7 @@ let openProfileModals = document.querySelectorAll('.my-profile');
 
 
 for (let i = 0; i < openProfileModals.length; i++) {
-  openProfileModals[i].addEventListener('click', function() {
+  openProfileModals[i].addEventListener('click', function () {
     let sectionsTitle = document.querySelector(".card_number");
     sectionsTitle.textContent = tempKey;
 
@@ -484,9 +485,9 @@ for (let i = 0; i < openProfileModals.length; i++) {
     let initials = (userData.userName[0] || "") + (userData.surname[0] || "");
     initials_main.textContent = initials;
     initials_sub.textContent = userData.userName + " " + userData.surname;
-  
+
     modalProfile.classList.add('fade-in');
-    setTimeout(function(){
+    setTimeout(function () {
       modalProfile.classList.add('modal_profile_test');
       body.classList.add('body_block');
       modalProfile.classList.remove('fade-in');
@@ -494,14 +495,14 @@ for (let i = 0; i < openProfileModals.length; i++) {
   });
 }
 
-modalProfile.addEventListener('click', function(event) {
+modalProfile.addEventListener('click', function (event) {
   if (event.target === modalProfile) {
-      modalProfile.classList.remove('modal_profile_test');
-      body.classList.remove('body_block');
+    modalProfile.classList.remove('modal_profile_test');
+    body.classList.remove('body_block');
   }
 });
 
-closeProfile.addEventListener('click', function() {
+closeProfile.addEventListener('click', function () {
   modalProfile.classList.remove('modal_profile_test');
   body.classList.remove('body_block');
 });
@@ -511,7 +512,7 @@ const copyIcon = document.getElementById('copy-icon');
 const cardNumber = document.querySelector(".card_number");
 const copyFeedback = document.getElementById('copy-feedback');
 
-copyIcon.addEventListener('click', function() {
+copyIcon.addEventListener('click', function () {
   const range = document.createRange();
   range.selectNode(cardNumber);
   window.getSelection().addRange(range);
@@ -525,56 +526,91 @@ copyIcon.addEventListener('click', function() {
 
 // card buy menu
 
-
-
 buyButtons.forEach(button => {
-  button.addEventListener('click', function() {
-      let bookId = this.getAttribute("data-book-id");
-      console.log("get" + bookId);
+  button.addEventListener('click', function () {
+    let bookId = this.getAttribute("data-book-id");
+    console.log("get" + bookId);
+    let storedValue = localStorage.getItem(tempKey);
+    let userData = JSON.parse(storedValue);
+    if (!userData.enteredCard && checkIfLoggedIn) {
+      modal_books.classList.add('fade-in');
+      setTimeout(function () {
+        modal_books.classList.add('modal_buy_books');
+        body.classList.add('body_block');
+        modal_books.classList.remove('fade-in');
+      }, 200);
+    } else if (checkIfLoggedIn && userData.enteredCard) {
+      updateBookValue(bookId)
+    } else {
+      modal_login.classList.add('modal_vis');
+      body_login.classList.add('body_block');
+    }
 
-      if (checkIfLoggedIn) {
-              modal_books.classList.add('fade-in');
-              setTimeout(function() {
-                  modal_books.classList.add('modal_buy_books');
-                  body.classList.add('body_block');
-                  modal_books.classList.remove('fade-in');
-              }, 200);
-      } else {
-          modal_login.classList.add('modal_vis');
-          body_login.classList.add('body_block');
+
+
+    document.getElementById("cardForm").addEventListener("submit", function (event) {
+      event.preventDefault();
+      let storedValue = localStorage.getItem(tempKey);
+      let userData = JSON.parse(storedValue);
+      userData.enteredCard = true;
+      try {
+        localStorage.setItem(tempKey, JSON.stringify(userData));
+        console.log("Data successfully updated in local storage.");
+        updateBookValue(bookId);
+        modal_books.classList.remove('modal_buy_books');
+        body.classList.remove('body_block');
+      } catch (error) {
+        console.error("An error occurred while updating data in local storage:", error);
       }
+    });
   });
 });
+
+function updateBookValue(id) {
+  let storedValue = localStorage.getItem(tempKey);
+  let userData = JSON.parse(storedValue);
+  if (!userData.numberOfBook) {
+    userData.numberOfBook = [];
+  }
+  userData.numberOfBook.push(id);
+  userData.booksOwn += 1;
+  try {
+    localStorage.setItem(tempKey, JSON.stringify(userData));
+    console.log("Data successfully updated in local storage.");
+
+    const selector = `button[data-book-id="${id}"]`;
+    console.log("Selector:", selector);
+
+    const button = document.querySelector(selector);
+    console.log("Button:", button);
+
+    if (button) {
+      button.classList.remove("buy");
+      button.classList.add("own-button");
+      button.disabled = true;
+      button.textContent = 'Own';
+    }
+  } catch (error) {
+    console.error("An error occurred while updating data in local storage:", error);
+  }
+}
 
 
 
 let close_buy_button = document.getElementById('close-buy-button');
-
-close_buy_button.addEventListener('click', function() {
-    modal_books.classList.remove('modal_buy_books');
-    body.classList.remove('body_block');
-    resetFormFields();
+close_buy_button.addEventListener('click', function () {
+  modal_books.classList.remove('modal_buy_books');
+  body.classList.remove('body_block');
+  resetFormFields();
 });
 
 
-modal_books.addEventListener('click', function(event) {
+modal_books.addEventListener('click', function (event) {
   if (event.target === modal_books) {
-      modal_books.classList.remove('modal_buy_books');
-      body.classList.remove('body_block');
-      resetFormFields();
+    modal_books.classList.remove('modal_buy_books');
+    body.classList.remove('body_block');
+    resetFormFields();
   }
 });
 
 
-
-// get card numbers
-
-let getBook = document.getElementById("buy-book");
-document.addEventListener("DOMContentLoaded", function () {
-  getBook.addEventListener("click", function (event) {
-    event.preventDefault();
-
-    console.log("test")
-
-  });
-});
