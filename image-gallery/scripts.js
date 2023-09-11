0ed46f7511519885e0f62f914098bb4c9cdb617c
nestserka <nechatherina@gmail.com>
2023-09-11 11:33:55 +0900

    feat : add grid layout for the page

--- image-gallery/scripts.js
+++ image-gallery/scripts.js
@@ -1,3 +1,4 @@
+let currentPage = 1; 
 let url = 'https://api.unsplash.com/search/photos?query=spring&per_page=30&orientation=landscape&client_id=SouHY7Uul-OxoMl3LL3c0NkxUtjIrKwf3tsGk1JaiVo';
 let isSearch = false;
 
@@ -11,7 +12,8 @@ async function getData(){
 
     const response = await fetch(url);
     const data = await response.json();
-    console.log(data);
+    console.log(data.total_pages);
+
     createGallery(data);
 }
 
@@ -20,12 +22,21 @@ function createGallery(data){
     galleryContainer.innerHTML = '';
     data.results.map(function(element){
         let thumbUrl = element.urls.regular;
+        let width = element.width;
+        let height = element.height;
+        const div = document.createElement('div');
+        if (parseInt(width) < parseInt(height)) {
+            div.classList.add('horizontal')
+        } else if (parseInt(width) > 5500){
+            div.classList.add('vertical')
+        } 
         const img = document.createElement('img');
         img.classList.add('gallery-img')
         img.src = thumbUrl;
         img.alt = `image`;
-        console.log("test" +img);
-        galleryContainer.append(img);
+       
+        galleryContainer.append(div);
+        div.appendChild(img);
     });
 }
 
@@ -37,7 +48,7 @@ document.addEventListener('DOMContentLoaded', function () {
         if (search_input.value === "") {
             return;
         } else {
-            url = `https://api.unsplash.com/search/photos?query=${search_input.value}&per_page=30&orientation=landscape&client_id=SouHY7Uul-OxoMl3LL3c0NkxUtjIrKwf3tsGk1JaiVo`;
+            url = `https://api.unsplash.com/search/photos?query=${search_input.value}&per_page=21&client_id=SouHY7Uul-OxoMl3LL3c0NkxUtjIrKwf3tsGk1JaiVo`;
             getData();
         }
     }
@@ -56,5 +67,10 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 
-
-
+// document.addEventListener('DOMContentLoaded', function () {
+//     let btnLoad = document.querySelector('.button-load');
+//     btnLoad.addEventListener('click', function() {
+//         url = url.concat("&page=2");
+//         getData();
+//     })
+// });
